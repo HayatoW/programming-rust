09162134c9c3ef59fcd32a862e1096808df297d2
Hayato Watanabe <solideye@me.com>
2026-04-26 07:10:49 +0000

    Initialize actix-gcd project with GCD calculator functionality
    - Created Cargo.toml for project configuration with dependencies on actix-web and serde.
    - Implemented main server in src/main.rs to handle GCD calculations.
    - Added GET endpoint for the index page with a form to input numbers.
    - Added POST endpoint to compute the GCD and return the result.
    - Included a GCD function using the Euclidean algorithm.
    - Added unit tests for the GCD function.

---
+++ actix-gcd/src/main.rs
@@ -0,0 +1,74 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+#[actix_web::main]
+async fn main() {
+    let server = HttpServer::new(|| App::new().service(get_index).service(post_gcd));
+
+    println!("Serving on http://localhost:3000...");
+    server
+        .bind("127.0.0.1:3000")
+        .expect("error binding server to address")
+        .run()
+        .await
+        .expect("error running server");
+}
+
+#[get("/")]
+async fn get_index() -> impl Responder {
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
+        <title>GCD Calculator</title>
+            <form action="/gcd" method="post">
+            <input type="text" name="n"/>
+            <input type="text" name="m"/>
+            <button type="submit">Compute GCD</button>
+        </form>
+        "#,
+    )
+}
+
+#[post("/gcd")]
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response = format!(
+        "The greatest common divisor of the numbers {} and {} is <b>{}</b>",
+        form.n,
+        form.m,
+        gcd(form.n, form.m)
+    );
+
+    HttpResponse::Ok().content_type("text/html").body(response)
+}
+
+/// https://en.wikipedia.org/wiki/Euclidean_algorithm
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+}
