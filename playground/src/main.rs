962fba90d264f4cf69a9acfae3933e76fdc35e54
Hayato Watanabe <solideye@me.com>
2026-05-04 05:55:27 +0000

    Add chapter 06 documentation on ranges and match expressions
    - Created chapter-06.md to introduce concepts of half-open and closed ranges in Rust.
    - Included examples of using match expressions as function and macro arguments.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,34 +1,5 @@
 // 検証用の書き捨て
 
-struct S<'a, 'b> {
-    x: &'a i32,
-    y: &'b i32,
-}
-
 fn main() {
-    let x = 10;
-    let r;
-    {
-        let y = 20;
-        {
-            let s = S { x: &x, y: &y };
-            r = s.x;
-        }
-    }
-    println!("{}", r);
-}
-
-struct StringTable {
-    elements: Vec<String>,
-}
-
-impl StringTable {
-    fn find_by_prefix(&self, prefix: &str) -> Option<&String> {
-        for i in 0..self.elements.len() {
-            if self.elements[i].starts_with(prefix) {
-                return Some(&self.elements[i]);
-            }
-        }
-        None
-    }
+    let is_even = |x: u64| -> bool { x % 2 == 0 };
 }
