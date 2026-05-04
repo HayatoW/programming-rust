bbb7b2080ad5446f1406e9732a8870f92d79be74
Hayato Watanabe <solideye@me.com>
2026-05-04 18:41:02 +0000

    Add polynomial struct and sine polynomial evaluation in main.rs
    - Introduced a new `Polynomial` struct to represent N-1 degree polynomials with methods for creation and evaluation.
    - Updated `main.rs` to include an example of creating a sine polynomial and validating its evaluation at specific points.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -6,6 +6,11 @@ fn main() {
 
     assert_eq!(*e.least, -3);
     assert_eq!(*e.greatest, 48);
+
+    let sine_poly = Polynomial::new([0.0, 1.0, 0.0, -1.0 / 6.0, 0.0, 1.0 / 120.0]);
+
+    assert_eq!(sine_poly.eval(0.0), 0.0);
+    assert!((sine_poly.eval(std::f64::consts::FRAC_PI_2) - 1.).abs() < 0.005);
 }
 
 struct Extrema<'elt> {
@@ -26,4 +31,25 @@ fn find_extrema(slice: &[i32]) -> Extrema<'_> {
         }
     }
     Extrema { greatest, least }
+}
+
+/// N-1 次の多項式を表す。
+struct Polynomial<const N: usize> {
+    /// 多項式の係数。
+    coefficients: [f64; N],
+}
+
+impl<const N: usize> Polynomial<N> {
+    fn new(coefficients: [f64; N]) -> Self {
+        Self { coefficients }
+    }
+
+    fn eval(&self, x: f64) -> f64 {
+        let mut sum = 0.0;
+        for i in (0..N).rev() {
+            sum = self.coefficients[i] + x * sum;
+        }
+
+        sum
+    }
 }
