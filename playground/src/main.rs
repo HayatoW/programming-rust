d01a1b75183a4bf2913e4f4d92bf5161812e6760
Hayato Watanabe <solideye@me.com>
2026-05-09 02:40:03 +0000

    Refactor main.rs to introduce Appellation and RcBox structures with Drop implementation
    - Added the Appellation struct to manage names and nicknames, including a custom Drop implementation for resource cleanup.
    - Introduced the RcBox struct to demonstrate reference counting with a Display trait object.
    - Updated the main function to showcase the usage of these new structures and their functionalities.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,7 +1,52 @@
 //! 検証用の書き捨て
 
-fn main() {
-    let mut floats = [4.0, 5.0, 1.0, 2.0, 3.0];
+use std::fmt::Display;
 
-    floats.sort();
+fn main() {
+    let mut a = Appellation {
+        name: "Zeus".to_string(),
+        nicknames: vec![
+            "cloud collector".to_string(),
+            "king of the gods".to_string(),
+        ],
+    };
+    println!("before assignment");
+    a = Appellation {
+        name: "Hera".to_string(),
+        nicknames: vec![],
+    };
+    println!("at end of block");
+
+    let boxed_lunch: RcBox<String> = RcBox {
+        ref_count: 1,
+        value: "lunch".to_string(),
+    };
+
+    let boxed_displayable: &RcBox<dyn Display> = &boxed_lunch;
+
+    display(&boxed_lunch);
+}
+
+struct Appellation {
+    name: String,
+    nicknames: Vec<String>,
+}
+
+impl Drop for Appellation {
+    fn drop(&mut self) {
+        print!("Dropping {}", self.name);
+        if !self.nicknames.is_empty() {
+            print!(" (AKA {})", self.nicknames.join(", "));
+        }
+        println!("");
+    }
+}
+
+struct RcBox<T: ?Sized> {
+    ref_count: usize,
+    value: T,
+}
+
+fn display(boxed: &RcBox<dyn Display>) {
+    println!("For your enjoyment: {}", &boxed.value);
 }
