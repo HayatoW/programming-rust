ced2eb69926f6f713010a915ce93445df5102e1e
Hayato Watanabe <solideye@me.com>
2026-05-09 05:07:46 +0000

    Refactor main.rs to implement Selector struct with Deref and DerefMut traits
    - Introduced the Selector struct to manage a collection of elements with a current index.
    - Implemented Deref and DerefMut traits for Selector to enable easy access and modification of the current element.
    - Updated the main function to demonstrate the usage of the Selector struct and its functionalities.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,52 +1,35 @@
 //! 検証用の書き捨て
 
-use std::fmt::Display;
+use std::ops::{Deref, DerefMut};
 
 fn main() {
-    let mut a = Appellation {
-        name: "Zeus".to_string(),
-        nicknames: vec![
-            "cloud collector".to_string(),
-            "king of the gods".to_string(),
-        ],
-    };
-    println!("before assignment");
-    a = Appellation {
-        name: "Hera".to_string(),
-        nicknames: vec![],
-    };
-    println!("at end of block");
-
-    let boxed_lunch: RcBox<String> = RcBox {
-        ref_count: 1,
-        value: "lunch".to_string(),
+    let mut s = Selector {
+        elements: vec!['x', 'y', 'z'],
+        current: 2,
     };
 
-    let boxed_displayable: &RcBox<dyn Display> = &boxed_lunch;
+    assert_eq!(*s, 'z');
+    assert!(s.is_alphabetic());
 
-    display(&boxed_lunch);
+    *s = 'w';
+    assert_eq!(s.elements, ['x', 'y', 'w']);
 }
 
-struct Appellation {
-    name: String,
-    nicknames: Vec<String>,
+struct Selector<T> {
+    elements: Vec<T>,
+    current: usize,
 }
 
-impl Drop for Appellation {
-    fn drop(&mut self) {
-        print!("Dropping {}", self.name);
-        if !self.nicknames.is_empty() {
-            print!(" (AKA {})", self.nicknames.join(", "));
-        }
-        println!("");
+impl<T> Deref for Selector<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.elements[self.current]
     }
 }
 
-struct RcBox<T: ?Sized> {
-    ref_count: usize,
-    value: T,
-}
-
-fn display(boxed: &RcBox<dyn Display>) {
-    println!("For your enjoyment: {}", &boxed.value);
+impl<T> DerefMut for Selector<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.elements[self.current]
+    }
 }
