2c807e9e938236ff8fd1c9015a653610ee11948d
Hayato Watanabe <solideye@me.com>
2026-05-10 09:10:03 +0000

    Refactor main.rs to remove HashSet usage and implement call_twice function
    - Eliminated the use of HashSet for partitioning squares and simplified the main function.
    - Introduced a new call_twice function that executes a closure twice, demonstrating a different functional approach.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,16 +1,12 @@
 //! 検証用の書き捨て
 
-use std::collections::HashSet;
-
 fn main() {
-    let squares = [4, 9, 16, 25, 36, 49, 64];
-    let (powers_of_two, impure): (HashSet<i32>, HashSet<i32>) =
-        squares.iter().partition(|&n| n & (n - 1) == 0);
+    let my_str = "hello".to_string();
+    let f = drop(my_str);
+    call_twice(f);
 }
 
-type GenericError = Box<dyn std::error::Error + Send + Sync + 'static>;
-type GenericResult<T> = Result<T, GenericError>;
-
-fn parse_i32_bytes(b: &[u8]) -> GenericResult<i32> {
-    Ok(std::str::from_utf8(b)?.parse::<i32>()?)
+fn call_twice<F: Fn()>(closure: F) {
+    closure();
+    closure();
 }
