d647bc6edf272060f4add5ed1af803b8ecaf6f58
Hayato Watanabe <solideye@me.com>
2026-05-12 23:59:04 +0000

    Update dependencies and refactor main.rs to utilize random number generation and complex numbers
    - Added the `rand` crate to Cargo.toml for random number generation.
    - Refactored main.rs to generate a vector of random lengths and introduced a new `escape_time` function using the `num` crate for complex number calculations.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,12 +1,20 @@
 //! 検証用の書き捨て
 
+use num::Complex;
+use rand::random;
+use std::iter::{from_fn, successors};
+
 fn main() {
-    let my_str = "hello".to_string();
-    let f = drop(my_str);
-    call_twice(f);
+    let lengths: Vec<f64> = from_fn(|| Some((random::<f64>() - random::<f64>()).abs()))
+        .take(1000)
+        .collect();
 }
 
-fn call_twice<F: Fn()>(closure: F) {
-    closure();
-    closure();
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let zero = Complex { re: 0.0, im: 0.0 };
+    successors(Some(zero), |&z| Some(z * z + c))
+        .take(limit)
+        .enumerate()
+        .find(|(_i, z)| z.norm_sqr() > 4.0)
+        .map(|(i, _z)| i)
 }
