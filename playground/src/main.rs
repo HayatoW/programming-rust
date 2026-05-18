64afca9b0ad6dc5809ca70c38c65f00a9a579cd5
Hayato Watanabe <solideye@me.com>
2026-05-18 23:28:02 +0000

    Refactor main.rs to implement a number parsing function using Peekable iterator
    - Removed the BTreeMap example and replaced it with a `parse_number` function that processes characters from a string, demonstrating the use of the Peekable iterator.
    - Updated the main function to test the new parsing functionality with assertions on number extraction.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,29 +1,28 @@
 //! 検証用の書き捨て
 
-use std::collections::BTreeMap;
+use std::iter::Peekable;
+
+fn parse_number<I>(tokens: &mut Peekable<I>) -> u32
+where
+    I: Iterator<Item = char>,
+{
+    let mut n = 0;
+    loop {
+        match tokens.peek() {
+            Some(r) if r.is_digit(10) => {
+                n = n * 10 + r.to_digit(10).unwrap();
+            }
+            _ => return n,
+        }
+        tokens.next();
+    }
+}
 
 fn main() {
-    // 都市とそこにある公園のテーブル。それぞれの値はベクタ
-    let mut parks = BTreeMap::new();
-
-    parks.insert("Portland", vec!["Mt. Tabor Park", "Forest Park"]);
-    parks.insert("Kyoto", vec!["Tadasu-no-Mori Forest", "Maruyama Koen"]);
-    parks.insert("Nashville", vec!["Percy Warner Park", "Dragon Park"]);
-
-    // すべての公園からなるベクタを作る。`values` は個々のベクタを返すイテレータを作る。
-    // `flatten` で各ベクタの要素をアイテムとして生成するイテレータを作る。
-    // 要素型のみ推論させる。
-    let all_parks: Vec<_> = parks.values().flatten().cloned().collect();
-
-    assert_eq!(
-        all_parks,
-        vec![
-            "Tadasu-no-Mori Forest",
-            "Maruyama Koen",
-            "Percy Warner Park",
-            "Dragon Park",
-            "Mt. Tabor Park",
-            "Forest Park"
-        ]
-    );
+    let mut chars = "226153980,1766319049".chars().peekable();
+    assert_eq!(parse_number(&mut chars), 226153980);
+    // `parse_number` はカンマを消費しないので、ここで消費する
+    assert_eq!(chars.next(), Some(','));
+    assert_eq!(parse_number(&mut chars), 1766319049);
+    assert_eq!(chars.next(), None);
 }
