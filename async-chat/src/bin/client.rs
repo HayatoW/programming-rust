094307d58b6440958b7290f546045e36d30a9627
Hayato Watanabe <solideye@me.com>
2026-07-09 10:49:22 +0000

    Add async-chat client with command input handling and server communication
    - Created client.rs binary with send_commands async function
    - Added async_chat::utils, async_std::prelude, async_std::io, and async_std::net imports
    - Implemented command-line interface displaying available commands (join GROUP, post GROUP MESSAGE)
    - Added instructions for closing connection using Control-D (Unix) or Control-Z (Windows)
    - Used BufReader to read command lines from stdin asynchronously
    - Parsed commands using

---
+++ async-chat/src/bin/client.rs
@@ -0,0 +1,26 @@
+use async_chat::utils::{self, ChatResult};
+use async_std::prelude::*;
+use async_std::{io, net};
+
+async fn send_commands(mut to_server: net::TcpStream) -> ChatResult<()> {
+    println!(
+        "Commands:\n\
+              join GROUP\n\
+              post GROUP MESSAGE...\n\
+              Type Control-D (on Unix) or Control-Z (on Windows) \
+              to close the connection."
+    );
+
+    let mut command_lines = io::BufReader::new(io::stdin()).lines();
+    while let Some(command_result) = command_lines.next().await {
+        let command = command_result?;
+        let request = match parse_command(&command) {
+            Some(request) => request,
+            None => continue,
+        };
+        utils::send_as_json(&mut to_server, &request).await?;
+        to_server.flush().await?
+    }
+
+    Ok(())
+}
