d79377e9216c1fcc038b10284dc61bc47b54aecf
Hayato Watanabe <solideye@me.com>
2026-08-04 14:27:09 +0000

    Add object literal support to json! macro with HashMap-based field insertion
    - Added pattern matching arm for object syntax {$key:tt : $value:tt, *} in json! macro
    - Implemented HashMap creation wrapped in Box for storing object fields
    - Added field insertion loop converting keys to strings and recursively expanding values with json! macro
    - Returns Json::Object variant containing the populated HashMap

--- json-macro/src/macros.rs
+++ json-macro/src/macros.rs
@@ -6,4 +6,11 @@ macro_rules! json {
     ([ $( $element:tt ),* ]) => {
         $crate::Json::Array(vec![ $( json!($element) ),* ])
     };
+    ({$($key:tt : $value: tt), *}) => {
+        {
+            let mut fields = $crate::macros::Box::new($crate::macros::HashMap::new());
+            $(fields.insert($crate::macros::ToString::to_string($key), json!($value));)*
+            $crate::Json::Object(fields)
+        }
+    };
 }
