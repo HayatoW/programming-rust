23e2b2386fa80d2cc2cff3513c38e0c00d7604fb
Hayato Watanabe <solideye@me.com>
2026-09-25 10:25:18 +0000

    書いてみた。ビルドはこける

--- build-meta/build.rs
+++ build-meta/build.rs
@@ -9,4 +9,12 @@ fn main() {
         .args(["rev-parse", "--short", "HEAD"])
         .output()
         .expect("git not found");
+    let git_hash = String::from_utf8_lossy(&output.stdout).trim().to_string();
+    println!("cargo::rustc-env=GIT_HASH={git_hash}");
+
+    let profile = std::env::var("PROFILE").unwrap_or_else(|_| "unknown".into());
+    println!("cargo::rustc-env=BUILD_PROFILE={profile}");
+
+    let target = std::env::var("TARGET").unwrap_or_else(|_| "unknown".into());
+    println!("cargo::rustc-env=BUILD_TARGET={target}");
 }
--- build-meta/src/main.rs
+++ build-meta/src/main.rs
@@ -1,3 +1,14 @@
+fn print_version() {
+    println!(
+        "{} {} (git:{} target:{} profile:{})",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION"),
+        env!("GIT_HASH"),
+        env!("BUILD_TARGET"),
+        env!("BUILD_PROFILE"),
+    );
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_version();
 }
